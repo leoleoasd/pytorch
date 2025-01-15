8d4f45f2d67a60f6e959603fde38664ca5db8e3a
Siddharth Kotapati <skotapati@apple.com>
2025-01-15 10:15:28 -0800

    MPS OpInfo core requirements

--- test/run_test.py
+++ test/run_test.py
@@ -29,6 +29,7 @@ from torch.testing._internal.common_utils import (
     IS_CI,
     IS_MACOS,
     IS_WINDOWS,
+    MACOS_VERSION,
     retry_shell,
     set_cwd,
     shell,
@@ -1784,6 +1785,10 @@ def get_selected_tests(options) -> List[str]:
             "test_nn",
             "inductor/test_mps_basic",
         ]
+        if MACOS_VERSION >= 15.0:
+            selected_tests += [
+                "test_ops",
+            ]
     else:
         # Exclude all mps tests otherwise
         options.exclude.extend(["test_mps", "test_metal"])
